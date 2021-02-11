9a1526b09a0428e5575a05861b6382f26404b8df
rajeevnaikte <rajeevnaikte@gmail.com>
2021-02-11 18:21:30 -0500

    view state changes update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-  extends:  [
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  ],
-  parserOptions:  {
-    sourceType:  'module',  // Allows for the use of imports
-  },
-  rules:  {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off'
-  }
+	parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+	extends: [
+		'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+	],
+	parserOptions: {
+		sourceType: 'module',  // Allows for the use of imports
+	},
+	rules: {
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/ban-ts-ignore':                 'off',
+		'@typescript-eslint/no-var-requires':               'off',
+		'@typescript-eslint/no-empty-function':             'off',
+		'@typescript-eslint/brace-style':                   ['error', 'allman'],
+		'import/prefer-default-export':                     'off',
+		'key-spacing':                                      ['error', {align: 'value'}],
+		'@typescript-eslint/no-use-before-define':          'off',
+		'indent':                                           ['error', 'tab']
+	}
 };
--- src/view/sheet.ts
+++ src/view/sheet.ts
@@ -3,59 +3,78 @@ import '../components/table';
 import './styles.css';
 
 const sheetView: ViewState = {
-  ux: 'bootstrap.card',
-  items: [{
-    ux: 'squid.table',
-    cssClass: 'bordered-table'
-  }, {
-    ux: 'bootstrap.btn-link',
-    text: 'Add column',
-    listeners: {
-      click: (viewModel: ViewModel, event: Event) => {
+	ux:    'bootstrap.card',
+	items: [
+	  {
+			ux:       'squid.table',
+			cssClass: 'bordered-table'
+		},
+		{
+			ux:    'bootstrap.btn-link',
+			state: {
+				text: 'Add column'
+			},
+			listeners: {
+				click: (viewModel: ViewModel, event: Event) =>
+				{
         viewModel.up('bootstrap.card')
-          ?.down('squid.table')?.[0].comp
+          ?.down('squid.table')?.[0]
           .addColumn({
-            ux: 'bootstrap.paragraph',
-            editable: true,
-            listeners: {
-              input (vm: ViewModel, e: Event) {
-                console.log(e);
-              }
-            }
+          	ux:        'bootstrap.paragraph',
+          	editable:  true,
+          	listeners: {
+          		input (vm: ViewModel, e: Event)
+          		{
+          			console.log(e);
+          		}
+          	}
           });
-      }
-    }
-  }, {
-    ux: 'bootstrap.btn-link',
-    text: 'Add Row',
-    listeners: {
-      click: (viewModel: ViewModel, event: Event) => {
+				}
+			}
+		},
+		{
+			ux:    'bootstrap.btn-link',
+			state: {
+			  text: 'Add Row'
+			},
+			listeners: {
+				click: (viewModel: ViewModel, event: Event) =>
+				{
         viewModel.up('bootstrap.card')
-          ?.down('squid.table')?.[0].comp
+          ?.down('squid.table')?.[0]
           .addRow();
-      }
-    }
-  }, {
-    ux: 'bootstrap.btn-link',
-    text: 'Delete column',
-    listeners: {
-      click: (viewModel: ViewModel, event: Event) => {
+				}
+			}
+		},
+		{
+			ux:    'bootstrap.btn-link',
+			state: {
+			  text: 'Delete column',
+			},
+			listeners: {
+				click: (viewModel: ViewModel, event: Event) =>
+				{
         viewModel.up('bootstrap.card')
           ?.down('squid.table')?.[0].comp
           .deleteColumn();
-      }
-    }
-  }, {
-    ux: 'bootstrap.btn-link',
-    text: 'Delete Row',
-    listeners: {
-      click: (viewModel: ViewModel, event: Event) => {
+				}
+			}
+		},
+		{
+			ux:    'bootstrap.btn-link',
+			state: {
+				text: 'Delete Row'
+			},
+			listeners: {
+				click: (viewModel: ViewModel, event: Event) =>
+				{
         viewModel.up('bootstrap.card')
           ?.down('squid.table')?.[0].comp
           .deleteRow();
-      }
-    }
-  }]
+				}
+			}
+		}
+	]
 };
 
 const genesis = UI.render(sheetView, 'root');
